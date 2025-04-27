645c3506fb2c73d4cd368120c466f95a8cb19cb5
Anna <suslova.as@phystech.edu>
2025-04-27 14:50:55 +0300

    Update Telegram_Weather_bot.01.py

--- Telegram_Weather_bot.01.py
+++ Telegram_Weather_bot.01.py
@@ -1,67 +1,47 @@
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import requests
 
 updater = Updater(token='...', use_context=True)
 
+
 dispatcher = updater.dispatcher
 
-MAIN_KEYBOARD = [['Понедельник', 'Вторник'], ['Среда', 'Четверг'], ['Пятница']]
 
-monday = '1'
-tuesday = '2'
-wednesday = '3'
-thursday = '4'
-friday = '5'
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Здравствуйте! Введите город, который вас интересует")
 
 
-def send(bot, chat_id, text, keyboard=None):
-    reply_keyboard = None
-    if keyboard:
-        reply_keyboard = ReplyKeyboardMarkup(
-            keyboard=keyboard,
-            reply_keyboard=True,
-            one_time_keyboard=True
-        )
-    bot.send_message(
-        chat_id=chat_id, text=text, reply_markup=reply_keyboard
-    )
-
-
-def start_action(update, context):
-    cid = update.effective_chat.id
-    send(context.bot, cid, 'Готов служить!', MAIN_KEYBOARD)
-
-
-def text_action(update, context):
-    text = update.message.text.lower()
-    cid = update.effective_chat.id
-    if text == 'понедельник':
-        send(context.bot, cid, monday)
-
-    elif text == 'вторник':
-        send(context.bot, cid, tuesday)
-
-    elif text == 'среда':
-        send(context.bot, cid, wednesday)
-
-    elif text == 'четверг':
-        send(context.bot, cid, thursday)
-
-    elif text == 'пятница':
-        send(context.bot, cid, friday)
-
-    else:
-        send(context.bot, cid, 'Моя твоя не понимать')
-
-
-start_handler = CommandHandler('start', start_action)
+start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 
-text_handler = MessageHandler(Filters.text & (~Filters.command), text_action)
-dispatcher.add_handler(text_handler)
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Возможные варианты, почему у вас не сработала программа:\n1) Вы ввели не город\n2) Такого города нет\n3) Не выполнены условия ввода')
+
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
+
+def weather(update, context):
+    city = update.message.text
+    #if city in data:
+    w = requests.get(f'https://wttr.in/{city}?format=3')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=w.text)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Для повторного запроса введите /start')
+    #else:
+    #    context.bot.send_message(chat_id=update.effective_chat.id, text='Упс! Кажется произошла ошибка! Повторите попытку или введите /help, чтобы узнать больше')
+
+
+weather_handler = MessageHandler(Filters.text & (~Filters.command), weather)
+dispatcher.add_handler(weather_handler)
 
 
 updater.start_polling()
 updater.idle()
+
